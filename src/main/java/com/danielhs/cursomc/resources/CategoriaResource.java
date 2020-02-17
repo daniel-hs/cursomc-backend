a0f34fc0208289af4f190234d7a717a6166edff9
daniel-hs <d1hs.henrique@gmail.com>
2020-02-17 19:20:03 -0300

    Testando GET com armazenamento em memória

--- src/main/java/com/danielhs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/danielhs/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.danielhs.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.danielhs.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-	public String listar() {
-		return "REST ESTÁ FUNCIONANDO !";
-	}
 	
+	@GetMapping
+	public List<Categoria> listarCategorias() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Informática");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+
+		return categorias;
+	}
 }
