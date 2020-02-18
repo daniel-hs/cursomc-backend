83ea5d3ef4da7e9ee8ba87a9e73a41ef80b87835
daniel-hs <d1hs.henrique@gmail.com>
2020-02-17 23:31:48 -0300

    Banco de dados H2 e criacao automatica da base de dados a partir dos objetos

--- src/main/java/com/danielhs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/danielhs/cursomc/resources/CategoriaResource.java
@@ -3,7 +3,12 @@ package com.danielhs.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +18,31 @@ import com.danielhs.cursomc.domain.Categoria;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+	List<Categoria> categorias = new ArrayList<>();
 	
 	@GetMapping
 	public List<Categoria> listarCategorias() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Informática");
-		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat1);
-		categorias.add(cat2);
-
 		return categorias;
 	}
+	
+	@PostMapping
+	public void salvarCategoria(@RequestBody Categoria categoria) {
+		System.out.println(categoria);
+		categorias.add(categoria);
+	}
+	
+	@PutMapping(value="/{id}")
+	public void alterarCategoria(@PathVariable Integer id, @RequestBody Categoria categoria) {
+		categorias.get(categoria.getId() - 1).setNome(categoria.getNome());;
+		
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public void excluirCategoria(@PathVariable Integer id) {
+		System.out.println(categorias.get(id));
+		categorias.remove(id);
+	}
+	
+	
+
 }
