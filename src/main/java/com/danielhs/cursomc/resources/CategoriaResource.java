f19980d13bd8004c9ebb173b4079661a3ba3c04a
daniel-hs <d1hs.henrique@gmail.com>
2020-02-17 23:48:36 -0300

    Criando repository e service para Categoria

--- src/main/java/com/danielhs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/danielhs/cursomc/resources/CategoriaResource.java
@@ -1,48 +1,29 @@
 package com.danielhs.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danielhs.cursomc.domain.Categoria;
+import com.danielhs.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-
-	List<Categoria> categorias = new ArrayList<>();
+	
+	@Autowired
+	private CategoriaService service;
 	
 	@GetMapping
-	public List<Categoria> listarCategorias() {
-		return categorias;
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
-	@PostMapping
-	public void salvarCategoria(@RequestBody Categoria categoria) {
-		System.out.println(categoria);
-		categorias.add(categoria);
-	}
-	
-	@PutMapping(value="/{id}")
-	public void alterarCategoria(@PathVariable Integer id, @RequestBody Categoria categoria) {
-		categorias.get(categoria.getId() - 1).setNome(categoria.getNome());;
-		
-	}
-	
-	@DeleteMapping(value="/{id}")
-	public void excluirCategoria(@PathVariable Integer id) {
-		System.out.println(categorias.get(id));
-		categorias.remove(id);
-	}
-	
+
 	
 
 }
---
+++ src/main/java/com/danielhs/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.danielhs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielhs.cursomc.domain.Categoria;
+import com.danielhs.cursomc.repositories.CategoriaRepository;
+
+//@Service 
+@Service
+public class CategoriaService {
+
+	//Automaticamente é instanciada pelo Spring, pelo mecanismo de independencia
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
